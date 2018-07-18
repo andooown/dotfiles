71a7ac1d4f360b9b5d6df2f3f6d4267d2d96feed
andooown <andooown.dev@gmail.com>
2018-07-18 19:36:54 +0900

    フォントサイズを変更

--- .hyper.js
+++ .hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 16,
 
     // font family with optional fallbacks
     fontFamily: '"Anonymous Pro for Powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
