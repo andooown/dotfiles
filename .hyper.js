a2dc262e7fad2da60071b7aa08d9afa700e5ce02
andooown <andooown.dev@gmail.com>
2018-12-02 01:15:01 +0900

    Install "hyper-afterglow"

--- .hyper.js
+++ .hyper.js
@@ -136,15 +136,14 @@ module.exports = {
   //   `project#1.0.1`
   plugins: [
     'hypercwd',
-    'hyper-confirm'
+    'hyper-confirm',
+    "hyper-afterglow"
   ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: [
-    "hyper-afterglow"
-  ],
+  localPlugins: [],
 
   keymaps: {
     // Example
