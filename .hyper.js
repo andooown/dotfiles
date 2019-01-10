ecd539e5a14faae0371a21bfe3b326b65912d9c1
andooown <andooown.dev@gmail.com>
2019-01-10 11:16:26 +0900

    Change hotkey for hyper-overlay

--- .hyper.js
+++ .hyper.js
@@ -129,6 +129,10 @@ module.exports = {
 
     overlay: {
       animate: false,
+      hotkeys: {
+        open: ['Control+Space'],
+        close: ['Control+Escape']
+      },
       primaryDisplay: true,
       size: {
         width: 1.0,
